1cc7f631bb3ea17ff8032216a44b9c58bdda4c30
Ilya Konstantinov <future@shiny.co.il>
2012-11-09 02:00:17 +0200

    Fix compilation as an import module

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := android-dl
@@ -5,7 +7,7 @@ LOCAL_SRC_FILES := \
 	android-dl.cpp \
 	jni.cpp \
 	common.cpp
-LOCAL_C_INCLUDES :=
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/inc
 # Avoid libstdc++ dependency -- we only use C++ as "better C"
 LOCAL_CFLAGS += -W -Wall -nodefaultlibs -fno-rtti -fno-exceptions
 LOCAL_LDLIBS := -llog
